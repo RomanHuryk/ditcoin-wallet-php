42e3a4773fbc93c6ed00700adbd12463f372dfeb
one4a11 <one4a11@msgsafe.io>
2018-06-10 21:10:13 +0100

    update example with new changes

--- example.php
+++ example.php
@@ -4,9 +4,15 @@ require "vendor/autoload.php";
 use Ditcoin\Wallet;
 
 $wallet = new Ditcoin\Wallet();
+# or with rpc authentification needed
+// $wallet = new Ditcoin\Wallet('127.0.0.1', 19092, 'ditrpcuser', 'rpcpasswd');
+
+# used when rpc wallet is started with `--wallet-dir` option
+// echo $wallet->create_wallet('dit_testy', 'testy');
+// echo $wallet->open_wallet('dit_testy', 'testy');
 
 $destination1 = (object) [
-    'amount' => '0.1',
+    'amount' => '1',
     'address' => '9RUGwFu3WGh3wAkeWWzMNiQXiW9ChYRpH974mDdrGcjpEcpPrz143oc9sV1W8YyAUwCztbfxt9usZSMVnSBwPxCaDXzhYWz'
 ];
 
@@ -16,3 +22,4 @@ $options = [
 
 echo $wallet->transfer($options);
 
+// echo $wallet->getAddress($options);
